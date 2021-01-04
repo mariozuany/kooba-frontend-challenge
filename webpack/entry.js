5cb183441218c8928504bf524d0df111d0480bc5
Mario Zuany <mario.zuanyneto@gmail.com>
2021-01-04 09:58:56 -0300

    Implements a grid layout debug viewer to help visualize the elements alignments

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,4 +1,5 @@
 module.exports = {
     main: './website/src/js/main.js',
-    homepage: './website/src/js/homepage.js'
+    homepage: './website/src/js/homepage.js',
+    challenge: './website/src/js/challenge.js'
 };
