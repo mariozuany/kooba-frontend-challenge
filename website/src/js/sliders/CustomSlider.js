1ef7778a75ce0324d55e26260b2412281c1bf176
Mario Zuany <mario.zuanyneto@gmail.com>
2021-01-04 12:44:19 -0300

    Layout and behavior adjustments

--- website/src/js/sliders/CustomSlider.js
+++ website/src/js/sliders/CustomSlider.js
@@ -1,47 +1,62 @@
 class CustomSlider {
     constructor() {
-        const interval = 3000;
-        const mobileBreakpoint = 960;
-
-        let visibleAmount = document.documentElement.clientWidth >= mobileBreakpoint ? 3 : 1;
-        let xAxis = 0;
-        let counterIndex = 0;
-
-        const articleList = document.querySelector('.article-list');
-
-        let articles = articleList.querySelectorAll('.article');
-        let articlesArr = Array.from(articles);
-        articlesArr.slice(0, visibleAmount).forEach(el => articleList.appendChild(el.cloneNode(true)));
-        articles = articleList.querySelectorAll('.article');
-
-        const articleWidth = articles[0].clientWidth + parseFloat(window.getComputedStyle(articles[0]).marginRight);
-        articleList.style.width = `${articleWidth * articles.length}px`;
-
-        setInterval(function() {
-
-            if (visibleAmount + counterIndex >= articles.length) {
-                xAxis = 0;
-                counterIndex = 0;
-                articleList.classList.remove('transition');
-                articleList.style.transform = `translate3d(0, 0, 0)`;
-
-                setTimeout(function() {
-                    articleList.classList.add('transition');
-                });
+        this.xAxis = 0;
+        this.counterIndex = 0;
+        this.interval = 3000;
+        this.mobileBreakpoint = 960;
+        this.visibleAmount = document.documentElement.clientWidth >= this.mobileBreakpoint ? 3 : 1;
+        this.articleList = document.querySelector('.article-list');
+        this.articles = document.querySelectorAll('.article');
+        this.articlesArr = Array.from(this.articles);
+        this.transformSettings = {
+            reset: 'translate3d(0, 0, 0)',
+            newAxis(xAxis) {
+                return `translate3d(${xAxis}px, 0, 0)`
             }
+        }
+    }
 
-            setTimeout(function() {
-                xAxis -= articleWidth;
-                counterIndex++;
-                articleList.style.transform = `translate3d(${xAxis}px, 0, 0)`;
-            });
 
-        }, interval);
+    resetPosition() {
+        console.log('reset position');
 
+        this.xAxis = 0;
+        this.counterIndex = 0;
+        this.articleList.classList.remove('transition');
+
+        setTimeout(() => {
+            this.articleList.style.transform = this.transformSettings.reset;
+            this.articleList.classList.add('transition');
+        });
+    }
+
+    toggleTimerIndicator() {
+        document.querySelector('.article.active').classList.remove('active');
+        this.articles[this.counterIndex].classList.add('active');
     }
 
     init() {
-        console.log('Custom Slider Init');
+        console.log('init customSlider');
+        this.articlesArr.slice(0, this.visibleAmount).forEach(el => this.articleList.appendChild(el.cloneNode(true)));
+        this.articles[0].classList.add('active');
+
+        setInterval(() => {
+            this.articles = this.articleList.querySelectorAll('.article');
+            this.articleWidth = this.articles[0].clientWidth + parseFloat(window.getComputedStyle(this.articles[0]).marginRight);
+            this.articleList.style.width = `${this.articleWidth * this.articles.length}px`;
+
+            if (this.visibleAmount + this.counterIndex >= this.articles.length) {
+                this.resetPosition();
+            }
+
+            setTimeout(() => {
+                this.xAxis -= this.articleWidth;
+                this.counterIndex++;
+                this.articleList.style.transform = this.transformSettings.newAxis(this.xAxis);
+                this.toggleTimerIndicator();
+            });
+
+        }, this.interval);
     }
 }
 
